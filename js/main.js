0a3bdfc1fbf33004bc81defe19477b7ecfc5a8e9
user-victoria <vicky.t.ma.27@gmail.com>
2023-10-02 20:34:01 +0200

    modifying

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
 // отримати від користувача через три prompt три числа
 // показати через alert середнє арифметичне цих чисел
 
-let num1 = +prompt('Write a first number');
-let num2 = +prompt('Write a second number');
-let num3 = +prompt('Write a third number');
+const firstNumber = +prompt('Write a first number');
+const secondNumber = +prompt('Write a second number');
+const thirdNumber = +prompt('Write a third number');
 
-if ((Number(num1) && Number(num2) && Number(num3)) || ((Number(num1) === 0) || (Number(num2) === 0)|| (Number(num3) === 0))) {
-    let average = (num1 + num2 + num3) / 3;
+if ((firstNumber && secondNumber && thirdNumber) || firstNumber === 0 || secondNumber === 0 || thirdNumber === 0) {
+    const average = (firstNumber + secondNumber + thirdNumber) / 3;
     alert(`Арифметичне середнє значення введених чисел = ${average}`);
 }
 else {
